b426281c139375fb0da70c5bbca786cd9b6d4f53
samirbhandari-123 <samirbhandari690@gmail.com>
2025-11-26 12:03:40 +0545

    Task 4 completed

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Task_4
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create objects of derived classes
+            Car myCar = new Car();
+            Bike myBike = new Bike();
+
+            // Call Display() method (concrete) from base class
+            myCar.Display();
+            myCar.StartEngine();
+            myCar.StopEngine();
+
+            Console.WriteLine(); // Blank line
+
+            myBike.Display();
+            myBike.StartEngine();
+            myBike.StopEngine();
         }
     }
 }
---
+++ Task 4/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal abstract class Vehicle
+    {
+        // Abstract methods
+        public abstract void StartEngine();
+        public abstract void StopEngine();
+
+        // Concrete method
+        public void Display()
+        {
+            Console.WriteLine("This is a vehicle");
+        }
+    }
+}
